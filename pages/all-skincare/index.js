26d45d2a3638c15f12f9a765b81fbdb86cb276a8
An Phan <antphan98@gmail.com>
2020-08-05 18:29:50 -0500

    fixed package.json issue

--- pages/all-skincare/index.js
+++ pages/all-skincare/index.js
@@ -1,16 +1,40 @@
 import Header from "../../components/Header/Header";
 import { skincare } from "../../data/skincare";
 import fetch from "node-fetch";
-
-const fetcher = (url) => fetch(url).then((res) => res.json());
+import { useState, useEffect } from "react";
 
 export default function allskincare() {
+  const [allSkincare, setAllSkincare] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await fetch("/api/skincare");
+      const data = await result.json();
+      setAllSkincare(data);
+      console.log(data);
+    }
+    fetchData();
+  }, []);
+
+  if (!allSkincare) {
+    return null;
+  }
+
   return (
     <div className="container">
       <Header />
       <div className="skin-search">
         <input type="text" className="search" />
       </div>
+
+      <h1>
+        {allSkincare &&
+          allSkincare.map((skincare, i) => (
+            <p key={i} skincare={skincare}>
+              {allSkincare.name}
+            </p>
+          ))}
+      </h1>
 
       <style jsx global>{``}</style>
     </div>
