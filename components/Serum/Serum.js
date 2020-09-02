a5f281944347b6526b6c4f8a707b9b931b18b2d9
An Phan <antphan98@gmail.com>
2020-09-02 18:10:28 -0500

    serum page working

---
+++ components/Serum/Serum.js
@@ -0,0 +1,42 @@
+import fetch from "node-fetch";
+import { useState, useEffect } from "react";
+import { Columns, Image, Container } from "react-bulma-components";
+
+const Serum = () => {
+  const [serums, setSerums] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await fetch("/api/skincare");
+      const data = await result.json();
+
+      const filteredSerums = data.filter((b) => {
+        if (b.producttype === "Serum") {
+          return true;
+        }
+
+        return false;
+      });
+      setSerums(filteredSerums);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <Columns>
+        {serums &&
+          serums.map((serum, i) => (
+            <Columns.Column key={i} size={4} skincare={serum}>
+              <Image src={serum.image}></Image>
+              <h1>{serum.name}</h1>
+              <p>{serum.price}</p>
+            </Columns.Column>
+          ))}
+        ;
+      </Columns>
+    </Container>
+  );
+};
+
+export default Serum;
---
+++ pages/serums/index.js
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Serum from "../../components/Serum/Serum";
+
+export default function serums() {
+  return (
+    <div className="container">
+      <Header />
+      <Serum />
+      <Footer />
+    </div>
+  );
+}
