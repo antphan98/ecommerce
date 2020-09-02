b0f533f38e93196b4d50de00ffc43196bb2a82a8
An Phan <antphan98@gmail.com>
2020-09-02 18:46:52 -0500

    face mask page done

---
+++ components/Mask/Mask.js
@@ -0,0 +1,40 @@
+import fetch from "node-fetch";
+import { useState, useEffect } from "react";
+import { Columns, Image, Container } from "react-bulma-components";
+
+const Mask = () => {
+  const [masks, setMasks] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const result = await fetch("/api/skincare");
+      const data = await result.json();
+
+      const filteredMasks = data.filter((b) => {
+        if (b.producttype === "Face Mask") {
+          return true;
+        }
+        return false;
+      });
+      setMasks(filteredMasks);
+    }
+
+    fetchData();
+  });
+
+  return (
+    <Container>
+      <Columns>
+        {masks &&
+          masks.map((mask, i) => (
+            <Columns.Column key={i} size={4} skincare={mask}>
+              <Image src={mask.image}></Image>
+              <h1>{mask.name}</h1>
+              <p>{mask.price}</p>
+            </Columns.Column>
+          ))}
+      </Columns>
+    </Container>
+  );
+};
+
+export default Mask;
---
+++ pages/masks/index.js
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Mask from "../../components/Mask/Mask";
+
+export default function exfoliator() {
+  return (
+    <div className="container">
+      <Header />
+      <Mask />
+      <Footer />
+    </div>
+  );
+}
